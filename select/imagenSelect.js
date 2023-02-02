37b6ff32e743533884b947d8afeeffb986d6bd35
MiriamAlvarad0 <alvaradoherasmiriamselena@gmail.com>
2023-02-01 19:19:10 -0700

    Select de imagen terminado 01 02 23

--- select/imagenSelect.js
+++ select/imagenSelect.js
@@ -9,10 +9,18 @@
     },
     computed:{
         imagenes(){
-            if(this.imagen="manzana"){
+            if(this.imagen=="manzana"){
                 return "https://www.cuerpomente.com/medio/2020/11/10/manzana_a1c5bdb0_1200x1200.jpg";
-            }else if (this.uva="uva"){
-                return "https://lossaboresdemexico.com/wp-content/uploads/2012/11/1ca887411c1d1264ecfed1b7ec2ec5a9.jpg"
+            }else if (this.imagen=="uva"){
+                return "https://lossaboresdemexico.com/wp-content/uploads/2012/11/1ca887411c1d1264ecfed1b7ec2ec5a9.jpg";
+            }else if(this.imagen=="pera"){
+                return "https://diarioroatan.com/wp-content/uploads/2018/08/pera.jpg";
+            }else if(this.imagen=="zana"){
+                return "https://image.made-in-china.com/2f0j10TvDEZkeahnuK/-Zanahoria-fresca-nuevos-cultivos-.jpg"
+            }else if(this.imagen=="cebo"){
+                return "https://greens.com.gt/wp-content/uploads/2020/04/Cebolla-Blanca.jpg"
+            }else{
+                return "";
             }
         }
     }
