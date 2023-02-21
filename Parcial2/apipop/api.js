57858211de424f4a34e8707a6980048a79fb0de7
JoanEduardoT <joane.torresespindola@gmail.com>
2023-02-21 09:30:11 -0600

    pokemones :p

--- Parcial2/apipop/api.js
+++ Parcial2/apipop/api.js
@@ -1,8 +1,11 @@
 Vue.createApp({
     data(){
         return {
-           pokemon: null
+            pokemones: []
         }
+    },
+    mounted(){ //solo se actualiza la pagina y muestra los pokemones sin necesidad de usar el boton
+        this.getPokemon();
     },
     methods:{
         async getPokemon(){
@@ -10,15 +13,22 @@ Vue.createApp({
             // post crear recursos
             // put modificar recusos
             // delete eliminar recursos
+            var i = 1;
+            while(i<=200){
+                const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+i,{
+                    method: "GET"
+                });
+                const pokemon= await response.json();
+                this.pokemones.push(pokemon);
+                i++;
 
-            const random=Math.floor(Math.random()*200); //math.floor es para que de entero, porq el math random da decimal!!
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+random,{
-                method:"GET"
-            });
+                //const random=Math.floor(Math.random()*200); //math.floor es para que de entero, porq el math random da decimal!!
+                //const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+random,{
+                    //method:"GET"
+                //});
+               
+            }
 
-
-            const data = await response.json();
-            this.pokemon=data;
         } 
     }
    
